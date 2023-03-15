a43aee9a6811506b2b2c6b9171f1f64361de7eaf
Malin <malin@MacBook-Pro-2.local>
2023-03-15 11:36:13 +0100

    added submit button to reload page

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,6 +6,8 @@ export const Result = ({ name, food, drink }) => {
       <p>Your name is {name}</p>
       <p>Your favourite food is {food}</p>
       <p>Your favourite drink is {drink}</p>
+      Thank you for taking this survey, Please submit if you are happy with your answers:
+      <button type="button" onClick={() => window.location.reload(false)}>Submit</button>
     </>
   );
 }
